9b0a69937af5514c261a41e3938afd28d266e791
LJY040310 <ljyoung04@gmail.com>
2024-02-07 18:00:25 +0900

    Work in progress

--- Python/sort.py
+++ Python/sort.py
@@ -1,5 +1,6 @@
 class Sort :
-
+    
+    @staticmethod
     def bubbleSort(lst):
         for i in range(len(lst)-1):
             for j in range(len(lst)-1-i):
@@ -7,6 +8,7 @@ class Sort :
                     lst[j], lst[j+1] = lst[j+1], lst[j]
         return lst
 
+    @staticmethod
     def insertionSort(lst):
         for i in range(1,len(lst)):
             key = lst[i]
@@ -16,9 +18,11 @@ class Sort :
                 j -= 1
             lst[j + 1] = key
         return lst
-
+    
+    @staticmethod
     def mergeSort():
         pass
 
+    @staticmethod
     def quickSort():
         pass
