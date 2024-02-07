95cc0b874069d7f9604daae7c5f8b27a63450769
LJY040310 <ljyoung04@gmail.com>
2024-02-07 17:52:33 +0900

    Work in progress

---
+++ Python/sort.py
@@ -0,0 +1,24 @@
+class Sort :
+
+    def bubbleSort(lst):
+        for i in range(len(lst)-1):
+            for j in range(len(lst)-1-i):
+                if lst[j] > lst[j+1]:
+                    lst[j], lst[j+1] = lst[j+1], lst[j]
+        return lst
+
+    def insertionSort(lst):
+        for i in range(1,len(lst)):
+            key = lst[i]
+            j = i - 1 
+            while j >= 0 and key < lst[j]:
+                lst[j + 1] = lst[j]
+                j -= 1
+            lst[j + 1] = key
+        return lst
+
+    def mergeSort():
+        pass
+
+    def quickSort():
+        pass
