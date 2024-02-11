f5461136212731a03efbc96427a0f136d6e73bc1
LJY040310 <ljyoung04@gmail.com>
2024-02-11 20:27:05 +0900

    Updated

---
+++ Python/Primes.py
@@ -0,0 +1,16 @@
+class Prime:
+
+    def isPrime(n):
+        for i in range(2, int(n**0.5)+1):
+            if n % i == 0:
+                return False
+        return True
+    def findPrime(n):
+        sieve = [True] * n
+
+        m = int(n ** 0.5)
+        for i in range(2,m+1):
+            if sieve[i] == True:
+                for j in range(i+i,n,i):
+                    sieve[j] = False
+        return [i for i in range(2,n) if sieve[i] == True]
--- Python/sort.py
+++ Python/sort.py
@@ -1,6 +1,6 @@
 class Sort :
     
-    @staticmethod
+    
     def bubbleSort(lst):
         for i in range(len(lst)-1):
             for j in range(len(lst)-1-i):
@@ -8,7 +8,7 @@ class Sort :
                     lst[j], lst[j+1] = lst[j+1], lst[j]
         return lst
 
-    @staticmethod
+    
     def insertionSort(lst):
         for i in range(1,len(lst)):
             key = lst[i]
@@ -19,10 +19,10 @@ class Sort :
             lst[j + 1] = key
         return lst
     
-    @staticmethod
+    
     def mergeSort():
         pass
 
-    @staticmethod
+    
     def quickSort():
         pass
